01567d6af33292d45d546d42b2383e4baf5bf479
Tristan Norton <tristan.m.norton@gmail.com>
2017-11-22 21:52:30 -0500

    Started work on Investment Portion. Once that's done it's all design work after that.

--- app_server/controllers/investment.js
+++ app_server/controllers/investment.js
@@ -1,3 +1,44 @@
+var babyFirstYear = {
+    bunInTheOven: {
+        price: '$350 (+$50/extra hour)*',
+        includes: [
+            'Up to 2 hours',
+            'Multiple locations/outfit changes',
+            'Flash drive with edited images',
+            'Online viewing gallery',
+            'Print release'
+        ]
+        bonus: [
+            '*+$100 for pregnancy announcement design',
+            '*Gender reveal options available upon request'
+        ]
+    },
+    welcomeHome: {
+        price: '$350 (+$50/extra hour)*',
+        includes: [
+            'Up to 4 hours',
+            'Half-day, in home, documentary style photoshoot',
+            'Flash drive with edited images',
+            'Online viewing gallery',
+            'Print release'
+        ]
+        bonus: [
+            '*+$100 for pregnancy announcement design',
+            '*Gender reveal options available upon request'
+        ]
+    },
+    imOne: {
+
+    },
+    bundleJoy: {
+
+    }
+}
+
+
+
+
 module.exports.investment = function(req, res) {
-    res.render('index',  { title: 'Investment' })
+    res.render('index',  { 
+        title: 'Investment', })
 };
