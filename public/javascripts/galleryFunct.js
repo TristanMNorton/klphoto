edc8ed83def3414a500245f337e877794fa428ea
Tristan Norton <tristan.m.norton@gmail.com>
2017-11-25 23:30:26 -0500

    more design work

--- public/javascripts/galleryFunct.js
+++ public/javascripts/galleryFunct.js
@@ -11,4 +11,9 @@ $('document').ready(function() {
         $('#portOverlay').css('opacity', '0');
         $('#portItemDisplay').attr('src', '');
     });
+
+    $(window).scroll(function(){
+        $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+      //250 is fade pixels
+      });
 });
